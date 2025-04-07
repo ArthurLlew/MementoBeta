4ed66b44f343a164974b1fe2a52f21629e8da440
ArthurLlew <non.fatum.mori@gmail.com>
2025-04-08 00:05:30 +0300

    Code refactor

--- src/main/java/net/arthurllew/mementobeta/block/entity/MementoBetaBlockEntities.java
+++ src/main/java/net/arthurllew/mementobeta/block/entity/MementoBetaBlockEntities.java
@@ -7,7 +7,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-public class MementoBetaBlockEntities {
+public abstract class MementoBetaBlockEntities {
     /**
      * Deferred Register for block entities.
      */
