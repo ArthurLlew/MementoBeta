c0df6214cffcc15bba43657c59901514da43d03d
ArthurLlew <non.fatum.mori@gmail.com>
2025-11-26 22:10:12 +0300

    Carver settings bug fix

--- src/main/java/net/arthurllew/mementobeta/world/levelgen/BetaChunkGeneratorSettings.java
+++ src/main/java/net/arthurllew/mementobeta/world/levelgen/BetaChunkGeneratorSettings.java
@@ -8,15 +8,19 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.RegistryFileCodec;
 import net.minecraft.world.level.block.Block;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * Stores custom beta chunk generator settings.
- * @param stoneBlock
- * @param sandstoneBlock
+ * @param stoneBlock stone equivalent block.
+ * @param sandstoneBlock sandstone equivalent block.
+ * @param grassBlocks grass blocks that can be met on the surface when carving caves.
+ * @param carverBlocks what blocks can be carved by caves (will include {@code grassBlocks})
  */
 public record BetaChunkGeneratorSettings(Block stoneBlock, Block sandstoneBlock,
-                                         List<Block> carverBlocks, List<Block> grassBlocks) {
+                                         List<Block> grassBlocks, List<Block> carverBlocks) {
     /**
      * Codec for reading file.
      */
@@ -26,10 +30,10 @@ public record BetaChunkGeneratorSettings(Block stoneBlock, Block sandstoneBlock,
                             BetaChunkGeneratorSettings::stoneBlock),
                     BuiltInRegistries.BLOCK.byNameCodec().stable().fieldOf("sandstone_block").forGetter(
                             BetaChunkGeneratorSettings::sandstoneBlock),
-                    BuiltInRegistries.BLOCK.byNameCodec().stable().listOf().fieldOf("carver_blocks").forGetter(
-                            BetaChunkGeneratorSettings::carverBlocks),
                     BuiltInRegistries.BLOCK.byNameCodec().stable().listOf().fieldOf("grass_blocks").forGetter(
-                            BetaChunkGeneratorSettings::grassBlocks)
+                            BetaChunkGeneratorSettings::grassBlocks),
+                    BuiltInRegistries.BLOCK.byNameCodec().stable().listOf().fieldOf("carver_blocks").forGetter(
+                            BetaChunkGeneratorSettings::carverBlocks)
             ).apply(settings, BetaChunkGeneratorSettings::new));
 
     /**
@@ -42,13 +46,15 @@ public record BetaChunkGeneratorSettings(Block stoneBlock, Block sandstoneBlock,
      * Slightly modified constructor
      */
     public BetaChunkGeneratorSettings(Block stoneBlock, Block sandstoneBlock,
-                                      List<Block> carverBlocks, List<Block> grassBlocks) {
+                                      List<Block> grassBlocks, List<Block> carverBlocks) {
         this.stoneBlock = stoneBlock;
         this.sandstoneBlock = sandstoneBlock;
-        this.carverBlocks = carverBlocks;
         this.grassBlocks = grassBlocks;
 
         // All grass blocks can be carved
-        this.carverBlocks.addAll(this.grassBlocks);
+        ArrayList<Block> allCarverBlocks = new ArrayList<>();
+        allCarverBlocks.addAll(carverBlocks);
+        allCarverBlocks.addAll(grassBlocks);
+        this.carverBlocks = Collections.unmodifiableList(allCarverBlocks);
     }
 }
